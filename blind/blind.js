83a022ed30600095378a91a61ce9c0f548a90fab
Martin Valen <martin.valen@kantega.no>
2023-01-02 15:50:06 +0100

    Added hints and made hideable structures

--- blind/blind.js
+++ blind/blind.js
@@ -2,6 +2,12 @@ window.addEventListener("click", function (e) {
 	if (e.target.id === "modal-overlay") {
 		hideModal();
 	}
+	if (e.target.id === "screen-reader-header") {
+		toggleHideable(".screen-readers");
+	}
+	if (e.target.id === "how-to-test-header") {
+		toggleHideable(".how-to-test");
+	}
 });
 
 function buy() {
@@ -17,6 +23,10 @@ function showModal() {
 
 function hideModal() {
 	document.getElementById("modal-overlay").style.display = "none";
+}
+
+function toggleHideable(hideableClass) {
+	document.querySelector(hideableClass).classList.toggle("hidden");
 }
 
 function validate() {
@@ -40,6 +50,7 @@ function validateNameInput(name) {
 	let validationErrorElem = document.getElementById("validation-error-name");
 	if (!name) {
 		validationErrorElem.innerText = "Navn er påkrevd";
+		validationErrorElem.style.display = "block"
 		return false;
 	}
 	return true;
@@ -49,6 +60,7 @@ function validateAddressInput(address) {
 	let validationErrorElem = document.getElementById("validation-error-address");
 	if (!address) {
 		validationErrorElem.innerText = "Addresse er påkrevd";
+		validationErrorElem.style.display = "block"
 		return false;
 	}
 	return true;
@@ -58,10 +70,12 @@ function validateEmailInput(email) {
 	let validationErrorElem = document.getElementById("validation-error-email");
 	if (!email) {
 		validationErrorElem.innerText = "Epost er påkrevd";
+		validationErrorElem.style.display = "block"
 		return false;
 	}
 	if (!String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
 		validationErrorElem.innerText = "Eposten er ikkje gyldig";
+		validationErrorElem.style.display = "block"
 		return false;
 	}
 	return true;
