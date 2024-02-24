395ebccfff1a9587cfcdb442db340df2c84f9a4a
Maiken <maikenfjellanger@gmail.com>
2024-02-24 22:38:36 +0100

    Svaksynt-side nytt design

---
+++ common/common.js
@@ -0,0 +1,25 @@
+function toggleReadMore() {
+  const element = document.getElementById("person");
+  const button = document.getElementById("read-more-button");
+
+  if (element.classList.contains("person-read-more")) {
+    element.classList.remove("person-read-more");
+    button.innerHTML = "Les mer";
+  } else {
+    element.classList.add("person-read-more");
+    button.innerHTML = "Les mindre";
+  }
+}
+
+function showHideGebyrgrunnlag(id) {
+  const element = document.getElementById("bygning-gebyrgrunnlag-" + id);
+  if (element.getAttribute("hidden") == null) {
+    element.setAttribute("hidden", "hidden");
+    document.getElementById("vis-gebyr-knapp-" + id).innerHTML =
+      "Legg til gebyr";
+  } else {
+    element.removeAttribute("hidden");
+    document.getElementById("vis-gebyr-knapp-" + id).innerHTML =
+      "Skjul gebyrer";
+  }
+}
