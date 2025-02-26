cc0e8ce260bd35a6976e61634475169344a59f67
Maiken Beate Fjellanger <maikenfjellanger@gmail.com>
2025-02-26 13:02:48 +0100

    Oppfriskning av stil og rydding i dialog-eksempel

--- blind/blind.js
+++ blind/blind.js
@@ -1,21 +1,24 @@
-let modalVisible = false;
-
 document.addEventListener("DOMContentLoaded", () => {
-	trapFocus(document.getElementById("modal"))
-})
+  const dialog = document.getElementById("rocket-dialog");
+  document.getElementById("openDialog").addEventListener("click", () => {
+      dialog.showModal();
+  });
+  document.getElementById("closeDialog").addEventListener("click", () => {
+      dialog.close();
+  });
 
-window.addEventListener("click", function (e) {
-  if (e.target.id === "modal-overlay") {
-    hideModal();
-  }
+  document.getElementById("buy").addEventListener("click", () => {
+    if (validate()) {
+      console.log("buy hei maiken yyyy");
+      dialog.close();
+    }
+  });
 });
 
 window.addEventListener("focusout", (e) => {
-  if (modalVisible) {
-    if (["input-name", "input-address", "input-email"].includes(e.target.id)) {
-      updateProgressBar();
-      validate(e.target.id);
-    }
+  if (["input-name", "input-address", "input-email"].includes(e.target.id)) {
+    updateProgressBar();
+    validate(e.target.id);
   }
 });
 
@@ -33,22 +36,6 @@ function updateProgressBar() {
   progressBar.style.width = (100 * progress) / 3 + "%";
 }
 
-function buy() {
-  if (validate()) {
-    hideModal();
-  }
-}
-
-function showModal() {
-  document.getElementById("modal-overlay").style.display = "flex";
-  document.getElementById("input-name").focus();
-  modalVisible = true;
-}
-
-function hideModal() {
-  document.getElementById("modal-overlay").style.display = "none";
-  modalVisible = false;
-}
 
 function validate(inputId = undefined) {
   let nameValidationMessage = "";
@@ -104,31 +91,3 @@ function validateEmailInput(email) {
   return "";
 }
 
-// Run trapFocus on a modal that you want to trap focus within.
-// Shamelessly stolen from https://hidde.blog/using-javascript-to-trap-focus-in-an-element/
-function trapFocus(element) {
-  var focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-  var firstFocusableEl = focusableEls[0];
-  var lastFocusableEl = focusableEls[focusableEls.length - 1];
-  var KEYCODE_TAB = 9;
-
-  element.addEventListener('keydown', function (e) {
-    var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-
-    if (!isTabPressed) {
-      return;
-    }
-
-    if (e.shiftKey) /* shift + tab */ {
-      if (document.activeElement === firstFocusableEl) {
-        lastFocusableEl.focus();
-        e.preventDefault();
-      }
-    } else /* tab */ {
-      if (document.activeElement === lastFocusableEl) {
-        firstFocusableEl.focus();
-        e.preventDefault();
-      }
-    }
-  });
-}
